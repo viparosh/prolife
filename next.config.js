d90ab5837dca3300ab228b087b550f63e37db49c
unknown <acemonkey2@gmail.com>
2022-04-29 10:58:12 +0800

    make it better

--- next.config.js
+++ next.config.js
@@ -11,8 +11,8 @@ module.exports = {
   // reactStrictMode: false,
   // webpack5: true,
   optimizeFonts: false,
-  // env: {
-  //   MONGO_URI:
-  //     'mongodb+srv://inutz123:inutz123@cluster0.vqd1t.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
-  // },
+  env: {
+    MONGO_URI:
+      'mongodb+srv://inutz123:inutz123@cluster0.vqd1t.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+  },
 }
